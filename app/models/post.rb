75576981d7c20723dc345678d376ac01fe9b7565
aevista <aevistax@gmail.com>
2016-07-14 22:17:50 -0500

    post tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,23 @@
 class Post < ActiveRecord::Base
-
+    attr_reader :body_size
     validates :title, presence: true, length: { maximum: 100 }
-    validates :body, presence: true,  length: { maximum: 249, 
-                                                                                tokenizer: lambda {|str| str.scan(/\s+|$/)}}
-
+    validates :body, presence: true
+    validate :body_token_count
     validates :user_id, presence: true                                                                            
+   
     belongs_to :user
     has_many :genres
     belongs_to :serial
     has_many :reactions
+
+    def body_token_count
+      if  WordsCounted.count(self.body).token_count > 250
+            errors.add(:post, "body cannot be greater than 250")
+        end
+    end
+
+    def body_size
+        WordsCounted.count(self.body).token_count
+    end
+    
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -22,5 +22,28 @@ class PostTest < ActiveSupport::TestCase
         assert_not @post.valid?
     end
 
+    test "should not post with 251 char" do
+        @post.body = ""
+        (1..251).each do 
+            @post.body += "a "
+        end
+        assert_not @post.valid?
+    end
 
+    test "should post with 250 char" do
+        @post.body = ""
+        (1..250).each do
+            @post.body += "a "
+        end
+        assert @post.valid?
+    end
+
+    test "shuld post with special characters" do
+         
+        @post.body =  "William S. Burroughs called this ``cut up theory.'' His approach was to take a page of text, divide it into quadrants, rearrange the quadrants, and then read the page across the divisions. He wrote this way; writing, cutting up, shuffling, publishing the result. Collage and randomness applied to words. He saw this as a way of escaping from a prison that words create for us, locking us down into one way of thinking: an idea echoed in Orwell's ``1984,'' where the purpose of NewSpeak was to make ThoughtCrime impossible by making it inexpressible: ``The Revolution will be complete when the language is perfect.''
+        William S. Burroughs called this ``cut up theory.'' His approach was to take a page of text, divide it into quadrants, rearrange the quadrants, and then read the page across the divisions. He wrote this way; writing, cutting up, shuffling, publishing the result. Collage and randomness applied to words. He saw this as a way of escaping from a prison that words create for us, locking us down into one way of thinking: an idea echoed in Orwell's ``1984,'' where the purpose of NewSpeak was to make ThoughtCrime impossible by making it inexpressible: ``The Revolution will be complete when the language is perfect.''
+        William S. Burroughs called this ``cut up theory.'' "
+       # puts "#{@post.body_size} size"
+        assert @post.valid?
+    end
 end
